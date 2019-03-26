5f1bf0e4f67d96e2222f37530edd7cc56ff0e888
Gopinath Rm <jayagopi1993@gmail.com>
2019-03-27 00:59:09 +0530

    Blog template created successfully.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
+import { FooterComponent } from './footer/footer.component';
+import { ItemComponent } from './item/item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StoriesComponent,
+    FooterComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
